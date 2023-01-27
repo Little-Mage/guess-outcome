6aa02d7afe47145c4783f2694b87fd0ac206e846
Little-Mage <lm4ssw@gmail.com>
2023-01-27 15:21:34 -0700

    Update count-func.js

--- count-func.js
+++ count-func.js
@@ -3,16 +3,16 @@ incorrect = 0;
 
 function correct_count() {
     correct += 1
-    document.getElementById('correct').innerHTML = 'Correct: ' + correct;
-    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect);
+    document.getElementById('correct').innerHTML = 'Correct: ' + correct
+    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect)
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
     document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + '%';
 }
 
 function incorrect_count() {
     incorrect += 1
-    document.getElementById('incorrect').innerHTML = 'Incorrect: ' + incorrect;
-    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect);
+    document.getElementById('incorrect').innerHTML = 'Incorrect: ' + incorrect
+    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect)
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
     document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + '%';
 }
