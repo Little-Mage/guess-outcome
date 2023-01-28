89dff1d04732f2fdb3b0557a5b2bd5300104a410
Little-Mage <lm4ssw@gmail.com>
2023-01-27 18:34:50 -0700

    expected %

--- count-func.js
+++ count-func.js
@@ -1,18 +1,27 @@
 correct = 0;
 incorrect = 0;
+dice_rolls = 0;
 
 function correct_count() {
     correct += 1
-    document.getElementById('correct').innerHTML = 'Correct: ' + correct
-    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect)
+    dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
-    document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %';
+    rolls = document.getElementById('rolls').value
+    expected = Math.round(dice_rolls/6)
+    document.getElementById('correct').innerHTML = 'Correct: ' + correct
+    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
+    document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %'
+    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected;
 }
 
 function incorrect_count() {
     incorrect += 1
-    document.getElementById('incorrect').innerHTML = 'Incorrect: ' + incorrect
-    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + (correct + incorrect)
+    dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
-    document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %';
+    rolls = document.getElementById('rolls').value
+    expected = Math.round(dice_rolls/6)
+    document.getElementById('correct').innerHTML = 'Correct: ' + correct
+    document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
+    document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %'
+    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected;
 }
