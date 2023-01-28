ceaf384999d27ae51e853893c4c517b71f8d2cda
Little-Mage <lm4ssw@gmail.com>
2023-01-27 19:02:34 -0700

    Update count-func.js

--- count-func.js
+++ count-func.js
@@ -6,7 +6,6 @@ function correct_count() {
     correct += 1
     dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
-    rolls = document.getElementById('rolls').value
     expected = (dice_rolls/6).toFixed(1)
     document.getElementById('correct').innerHTML = 'Correct: ' + correct
     document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
@@ -18,7 +17,6 @@ function incorrect_count() {
     incorrect += 1
     dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
-    rolls = document.getElementById('rolls').value
     expected = (dice_rolls/6).toFixed(1)
     document.getElementById('incorrect').innerHTML = 'Incorrect: ' + incorrect
     document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
