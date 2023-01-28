fb8a6279fa0b27715013b6bcdc16fbcf04f4dc8f
Little-Mage <lm4ssw@gmail.com>
2023-01-27 18:44:14 -0700

    expected decimal

--- count-func.js
+++ count-func.js
@@ -7,11 +7,11 @@ function correct_count() {
     dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
     rolls = document.getElementById('rolls').value
-    expected = Math.round(dice_rolls/6)
+    expected = dice_rolls/6
     document.getElementById('correct').innerHTML = 'Correct: ' + correct
     document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
     document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %'
-    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected;
+    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected.toFixed(1);
 }
 
 function incorrect_count() {
@@ -19,9 +19,9 @@ function incorrect_count() {
     dice_rolls += 1
     correct_p = Math.round((correct / (correct + incorrect)) * 100)
     rolls = document.getElementById('rolls').value
-    expected = Math.round(dice_rolls/6)
-    document.getElementById('correct').innerHTML = 'Correct: ' + correct
+    expected = dice_rolls/6
+    document.getElementById('incorrect').innerHTML = 'Incorrect: ' + incorrect
     document.getElementById('rolls').innerHTML = 'Dice rolls: ' + dice_rolls    
     document.getElementById('correct percent').innerHTML = 'Correct percent: ' + correct_p + ' %'
-    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected;
+    document.getElementById('expected').innerHTML = 'Expected correct: ' + expected.toFixed(1);
 }
